ecae93889271df698325d99c88524f5fa590c251
santiloopz <santilpzate@gmail.com>
2020-10-26 09:18:38 +0100

    workin on first component output

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ThreadSectionComponent } from '@features/components/thread-section/thread-section.component';
+
+const routes: Routes = [
+  { path: '', component: ThreadSectionComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
