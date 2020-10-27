ba7ff705a8f52889ff45efbf02b3e403be12b9e5
santiloopz <santilpzate@gmail.com>
2020-10-27 08:46:04 +0100

    added app core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppCoreModule } from '@core/app-core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    AppCoreModule,
     AppRoutingModule
+  ],
+  declarations: [
+    AppComponent
   ],
   bootstrap: [AppComponent]
 })
