a225e71579933ab80c50cc4fcea3e8a81418d1b6
santiloopz <santilpzate@gmail.com>
2020-12-08 14:35:45 +0100

    retrieving data from the store

--- src/app/features/components/thread-section/thread-section.component.ts
+++ src/app/features/components/thread-section/thread-section.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
-import { map, take } from 'rxjs/operators';
+import { map, skip, take } from 'rxjs/operators';
 import { Store } from '@ngrx/store';
 
 import { ThreadsService } from '@shared/services';
 import { threadsActions } from '@store/index';
 import { AppState } from '@store/models';
 import { mapEntry } from '@angular/compiler/src/output/map_util';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-thread-section',
@@ -16,22 +17,46 @@ import { mapEntry } from '@angular/compiler/src/output/map_util';
 })
 export class ThreadSectionComponent implements OnInit {
 
+  userName$!: Observable<string>;
+
   constructor(
     private threadsSrv: ThreadsService,
     private store: Store<{ appState: AppState }>
   ) { }
 
   ngOnInit(): void {
-    this.store.subscribe(console.log);
+    this.fetchUserThreads();
+    this.userName$ = this.store.pipe(
+      skip(1),
+      map(state => state.appState),
+      map(this.mapStateToUserName),
+    );
+  }
+
+  /**
+   * maps store state to user name
+   * @param state
+   * @private
+   */
+  private mapStateToUserName({ storeData, uiState }: AppState): string {
+    return storeData.participants[uiState.userId!].name;
+  }
+
+  /**
+   * Get the user threads from the store and send them
+   * to the reducer
+   * @private
+   */
+  private fetchUserThreads(): void {
     this.store
       .pipe(take(1))
       .subscribe((store: { appState: AppState }) => {
-      this.threadsSrv
-        .loadUserThreads(store.appState.storeData.user.id)
-        .subscribe(userTreads => {
-          this.store.dispatch(threadsActions.loadUserThreads({ payload: userTreads }));
-        });
-    });
+        this.threadsSrv
+          .loadUserThreads(store.appState.storeData.user.id)
+          .subscribe(userTreads => {
+            this.store.dispatch(threadsActions.loadUserThreads({ payload: userTreads }));
+          });
+      });
   }
 
 }
